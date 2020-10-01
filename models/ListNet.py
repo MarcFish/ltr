0a6972984b0e9ce8b0eb2fb2e7fc3614d5544acd
marcfish <marcfish@sea.com>
2020-10-01 19:07:06 +0800

    listnet

--- models/ListNet.py
+++ models/ListNet.py
@@ -11,13 +11,14 @@ parser.add_argument("--feature_num",type=int,default=46)
 parser.add_argument("--lr", type=float,default=1e-3)
 parser.add_argument("--batch",type=int,default=1024)
 parser.add_argument("--epochs",type=int,default=10)
+parser.add_argument("--k", type=int, default=5)
 
 arg = parser.parse_args()
 
 
-class RankNet(keras.Model):
+class ListNet(keras.Model):
     def __init__(self, hidden_units):
-        super(RankNet, self).__init__()
+        super(ListNet, self).__init__()
         self.net = keras.Sequential()
         for unit in hidden_units:
             self.net.add(keras.layers.Dense(unit))
@@ -26,43 +27,31 @@ class RankNet(keras.Model):
         self.net.add(keras.layers.Dense(1, activation='sigmoid'))
 
     def call(self, inputs):
-        f1 = inputs[0]
-        f2 = inputs[1]
-        diff = self.net(f1) - self.net(f2)
-        return diff
+        o = self.net(inputs)
+        return o
 
 
-def loss(rel, diff):
-    return -rel * diff + tf.math.softplus(diff)
+def loss(rel, out):
+    rel = tf.nn.softmax(rel, axis=-1)
+    return tf.math.reduce_sum(rel * tf.nn.log_softmax(out, axis=-1), axis=1)
 
 
 def get_data(data):
     r_ = list()
-    f1_ = list()
-    f2_ = list()
+    f_ = list()
     for q, qdict in tqdm(data.q_dict.items()):
-        rel = qdict['rel']
-        feature = qdict['feature']
-        for i in range(len(rel)):
-            for j in range(len(rel)):
-                r = rel[i] - rel[j]
-                y = r
-                if r < 0:
-                    r = 1
-                elif r == 0:
-                    r = 0.5
-                else:
-                    r = 0
-                r_.append(r)
-                f1_.append(feature[i])
-                f2_.append(feature[j])
-    return np.asarray(r_), np.stack(f1_), np.stack(f2_)
+        rel = np.asarray(qdict['rel'])
+        feature = np.asarray(qdict['feature'])
+        for i in range(arg.k):
+            b = np.random.choice(a=len(rel), size=arg.k)
+            r_.append(rel[b])
+            f_.append(feature[b])
+    return np.asarray(r_).astype(np.float32), np.stack(f_).astype(np.float32)
 
 
 data = Data(arg.file)
-r, f1, f2 = get_data(data)
+r, f = get_data(data)
 
-
-model = RankNet([128,128,128])
+model = ListNet([128, 128, 128])
 model.compile(loss=loss, optimizer=keras.optimizers.Adam(arg.lr))
-model.fit([f1, f2], r, epochs=arg.epochs, batch_size=arg.batch)
+model.fit(f, r, epochs=arg.epochs, batch_size=arg.batch)
